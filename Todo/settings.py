c086ce01b998ae384d2d120b780077a28b0201d9
Godstime01 <youngestkid01gmail.com>
2022-05-13 18:42:49 +0100

    fixed my env file

--- Todo/settings.py
+++ Todo/settings.py
@@ -1,14 +1,15 @@
 import environ
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -34,7 +35,7 @@ INSTALLED_APPS = [
     
     # third-party app
     "tailwind",
-    "django-browser-reload",
+    # "django-browser-reload",
     
     # my apps
     'todoapp',
